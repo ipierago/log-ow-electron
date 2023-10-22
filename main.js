4999e2513e5ef1b6a122e43fda5dd044f90e7397
Ian Pieragostini <ian.pieragostini@gmail.com>
2023-10-22 12:11:14 +0700

    fixed require statement

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
+//const { app, BrowserWindow } = require('@overwolf/ow-electron');
+
 const { app, BrowserWindow } = require('electron');
+
+console.log('hello world!');
 
 let mainWindow;
 
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    // Load index.html into the mainWindow
-    mainWindow.loadFile('index.html');
+  // Load index.html into the mainWindow
+  mainWindow.loadFile('index.html');
 
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 });
 
-app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.whenReady().then(() => {
+  const appID = process.env.OVERWOLF_APP_UID;
+  console.log(`AppID: ${appID}`);
+});
+
+console.log(`app.overwolf: ${app.overwolf}`);
+
+app.overwolf.packages.on('ready', (event, packageName, version) => {
+  console.log(
+    `app.overwolf.packages.on: event: ${JSON.stringify(
+      event
+    )}, packageName: ${packageName}, version: ${version}`
+  );
 });
